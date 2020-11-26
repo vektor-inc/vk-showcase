95b737effc73948ee971d3eb9e6285dfc7d771fa
drill-lancer <rickaddison7634@gmail.com>
2020-11-26 16:55:07 +0900

    権限を追加

--- inc/showcase/package/class-vk-showcase.php
+++ inc/showcase/package/class-vk-showcase.php
@@ -14,7 +14,23 @@ class VK_Showcase {
 	 * Constructor
 	 */
 	public function __construct() {
+		add_action( 'init', array( __CLASS__, 'add_role' ) );
 		add_action( 'init', array( __CLASS__, 'register_post_type' ) );
+		add_action( 'admin_init', array( __CLASS__, 'role_setting' ) );
+	}
+
+	/**
+	 * Add Role
+	 */
+	public static function add_role() {
+		global $wp_roles;
+		$wp_roles->add_role(
+			'creator',
+			__( 'Creator', 'vk-showcase' ),
+			array(
+				'read'    => true,
+			)
+		);
 	}
 
 	/**
@@ -29,9 +45,15 @@ class VK_Showcase {
 				'label'        => $vkfs_prefix . __( 'Showcase', 'vk-showcase' ),
 				'public'       => true,
 				'has_archive'  => true,
+				'capabilities' => array(
+					'edit_posts'   => 'showcase_capabilities',
+					'delete_posts' => 'showcase_capabilities',
+					'read'         => 'showcase_capabilities',
+				),
+				'map_meta_cap' => true,
 				'menu_icon'    => 'dashicons-screenoptions',
 				'show_in_rest' => true,
-				'supports'     => array( 'title', 'author', 'thumbnail', 'excerpt', 'custom-fields', 'editor' ),
+				'supports'     => array( 'title', 'author', 'thumbnail', 'excerpt', 'custom-fields' ),
 			)
 		);
 
@@ -43,6 +65,10 @@ class VK_Showcase {
 				'public'            => true,
 				'show_admin_column' => true,
 				'hierarchical'      => true,
+				'show_in_rest' => true,
+				'capabilities' => array(
+					'assign_terms' => 'showcase_capabilities',
+				),
 			)
 		);
 
@@ -54,6 +80,10 @@ class VK_Showcase {
 				'public'            => true,
 				'show_admin_column' => true,
 				'hierarchical'      => true,
+				'show_in_rest' => true,
+				'capabilities' => array(
+					'assign_terms' => 'showcase_capabilities',
+				),
 			)
 		);
 
@@ -65,9 +95,25 @@ class VK_Showcase {
 				'public'            => true,
 				'show_admin_column' => true,
 				'hierarchical'      => true,
+				'show_in_rest' => true,
+				'capabilities' => array(
+					'assign_terms' => 'showcase_capabilities',
+				),
 			)
 		);
 
+	}
+
+	/**
+	 * Role Setting
+	 */
+	public static function role_setting() {
+		global $wp_roles;
+		$wp_roles->add_cap( 'administrator', 'showcase_capabilities' );
+		$wp_roles->add_cap( 'editor', 'showcase_capabilities' );
+		$wp_roles->add_cap( 'author', 'showcase_capabilities' );
+		$wp_roles->add_cap( 'contributor', 'showcase_capabilities' );
+		$wp_roles->add_cap( 'creator', 'showcase_capabilities' );
 	}
 }
 new VK_Showcase();
