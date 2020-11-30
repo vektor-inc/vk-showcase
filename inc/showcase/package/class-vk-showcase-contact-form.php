456bfb0ee025e71aae7d27a4a4bb2cc6fbd0aa71
drill-lancer <rickaddison7634@gmail.com>
2020-11-30 13:27:37 +0900

    フォームテスト

--- inc/showcase/package/class-vk-showcase-contact-form.php
+++ inc/showcase/package/class-vk-showcase-contact-form.php
@@ -15,6 +15,7 @@ class VK_Showcase_Contact_Form {
 	 */
 	public function __construct() {
 		add_filter( 'the_content', array( __CLASS__, 'add_contact_button' ) );
+		add_filter( 'wpcf7_form_tag', array( __CLASS__, 'set_form' ), 11 );
 	}
 
 	/**
@@ -27,11 +28,12 @@ class VK_Showcase_Contact_Form {
 
 		$user_id       = get_userdata( $post->post_author )->ID;
 		$user_email    = get_the_author_meta( 'user_email', $user_id );
+		$user_name     = get_the_author_meta( 'nickname', $user_id );
 		$allow_contact = get_the_author_meta( 'allow_contact', $user_id );
 
 		if ( 'showcase' === get_post_type() ) {
 			if ( '1' === $allow_contact ) {
-				$contact_button .= '<a class="btn btn-primary text-center" href="' . $form_url . '?user_mail_address=' . $user_email . '">';
+				$contact_button .= '<a class="btn btn-primary text-center" href="' . $form_url . '?creator-name=' . $user_name . '&creator-email=' . $user_email . '">';
 				$contact_button .= __( 'この製作者に制作依頼をする', 'vk-showcase' );
 				$contact_button .= '</a>';
 			} else {
@@ -42,6 +44,27 @@ class VK_Showcase_Contact_Form {
 			}
 		}
 		return $content . $contact_button;
+	}
+
+	public static function set_form( $tag ) {
+		if ( ! is_array( $tag ) ) {
+			return $tag;
+		}
+		// ユーザー（顧客）のメールアドレス.
+		if( isset( $_GET['creator-name'] ) ) {
+			$name = $tag['name'];
+			if( $name == 'creator-name' ) {
+				$tag['values'] = (array) $_GET['creator-name'];
+			}
+		}
+		// ユーザー（顧客）のメールアドレス.
+		if( isset( $_GET['creator-email'] ) ) {
+			$name = $tag['name'];
+			if( $name == 'creator-email' ) {
+				$tag['values'] = (array) $_GET['creator-email'];
+			}
+		}
+		return $tag;
 	}
 }
 new VK_Showcase_Contact_Form();
