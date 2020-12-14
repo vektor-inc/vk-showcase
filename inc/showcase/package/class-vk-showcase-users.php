5fb68f117419d550c92d1fc4d717db75efda8e1c
drill-lancer <rickaddison7634@gmail.com>
2020-12-14 09:08:37 +0900

    add_creator_role

--- inc/showcase/package/class-vk-showcase-users.php
+++ inc/showcase/package/class-vk-showcase-users.php
@@ -14,10 +14,28 @@ class VK_Showcase_Users {
 	 * Constructor
 	 */
 	public function __construct() {
+		add_action( 'init', array( __CLASS__, 'add_creator_role' ), 10, 2 );
 		add_action( 'show_password_fields', array( __CLASS__, 'add_allow_contact' ) );
 		add_action( 'profile_update', array( __CLASS__, 'update_allow_contact' ), 10, 2 );
 		// add_filter( 'woocommerce_prevent_admin_access', '__return_false' );
 		// add_filter( 'woocommerce_disable_admin_bar', '__return_false' );
+	}
+
+	/**
+	 * Add Creator Role
+	 */
+	public static function add_creator_role() {
+		add_role(
+			'creator',
+			__( '製作者', 'vk-showcase' ),
+			array(
+				'read'         => true,
+				'edit_posts'   => true,
+				'delete_posts' => true,
+				'upload_files' => true,
+
+			),
+		);
 	}
 
 	/**
@@ -27,7 +45,7 @@ class VK_Showcase_Users {
 		global $profileuser;
 		$user_meta = get_user_meta( $profileuser->ID, 'allow_contact', true );
 
-		if ( current_user_can( 'contributor' ) && defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE ) {
+		if ( current_user_can( 'creator' ) && defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE ) {
 			?>
 			<tr>
 				<th scope="row"><?php _e( '制作問い合わせを受け付ける', 'vk-showcase' ); ?></th>
@@ -45,7 +63,7 @@ class VK_Showcase_Users {
 	 * Update Allow Contact on Profile
 	 */
 	public static function update_allow_contact( $user_id, $old_user_data ) {
-		if ( current_user_can( 'contributor' ) && defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE ) {
+		if ( current_user_can( 'creator' ) && defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE ) {
 			if ( isset( $_POST['allow_contact'] ) ) {
 				$allow_contact = true;
 			} else {
